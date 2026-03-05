b1a78a608fa7931ac1079d4a158e9ed10fc8430c
anthony fernando torres detquizan <anthonyfernandotorresdetquizan@MacBook-Pro-de-anthony-4183.local>
2026-03-05 11:33:15 -0500

    feat: mejoras

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  base: '/skills-portal',
   srcDir: 'src',
   site: 'https://aI-dev-innovators.github.io/skills-portal',
   markdown: {
